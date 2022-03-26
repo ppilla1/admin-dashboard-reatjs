2c29be48ecf719e112aa3c91e1dfd627d37f253d
Prashant Pillai <ppillai@Prashants-MacBook-Air-2.local>
2022-03-26 23:51:49 +0530

    React sidebar menu design

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,4 +1,17 @@
 import React from "react"
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
+import LogoutIcon from '@mui/icons-material/Logout'
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined'
+import SettingsApplicationsSharpIcon from '@mui/icons-material/SettingsApplicationsSharp'
+import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined'
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined'
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined'
+import AssessmentIcon from '@mui/icons-material/Assessment'
+import LocalShippingIcon from '@mui/icons-material/LocalShipping'
+import StoreIcon from '@mui/icons-material/Store'
+import CreditCardIcon from '@mui/icons-material/CreditCard'
+
 import "./Sidebar.scss"
 
 const Sidebar = () => {
@@ -11,13 +24,65 @@ const Sidebar = () => {
             <hr/>
             <div className="center">
                 <ul>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
+                    <p className="title">MAIN</p>
+                    <li>
+                        <DashboardIcon className="icon"/>
+                        <span>Dashboard</span>
+                    </li>
+                    <p className="title">LISTS</p>
+                    <li>
+                        <PersonOutlineOutlinedIcon className="icon"/>
+                        <span>Users</span>
+                    </li>
+                    <li>
+                        <StoreIcon className="icon"/>
+                        <span>Products</span>
+                    </li>
+                    <li>
+                        <CreditCardIcon className="icon"/>
+                        <span>Orders</span>
+                    </li>
+                    <li>
+                        <LocalShippingIcon className="icon"/>
+                        <span>Delivery</span>
+                    </li>
+                    <p className="title">USEFUL</p>
+                    <li>
+                        <AssessmentIcon className="icon"/>
+                        <span>Stats</span>
+                    </li>
+                    <li>
+                        <NotificationsOutlinedIcon className="icon"/>
+                        <span>Notifications</span>
+                    </li>
+                    <p className="title">SERVICE</p>
+                    <li>
+                        <SettingsSystemDaydreamOutlinedIcon className="icon"/>
+                        <span>System Health</span>
+                    </li>
+                    <li>
+                        <PsychologyOutlinedIcon className="icon"/>
+                        <span>Logs</span>
+                    </li>
+                    <li>
+                        <SettingsApplicationsSharpIcon className="icon"/>
+                        <span>Settings</span>
+                    </li>
+                    <p className="title">USER</p>
+                    <li>
+                        <AccountCircleOutlinedIcon className="icon"/>
+                        <span>Profile</span>
+                    </li>
+                    <li>
+                        <LogoutIcon className="icon"/>
+                        <span>Logout</span>
+                    </li>
                 </ul>
             </div>
-            <div className="bottom">Colot Opt.</div>
+            <div className="bottom">
+                <div className="colorOption"></div>
+                <div className="colorOption"></div>
+            </div>
         </div>
     )
 }
