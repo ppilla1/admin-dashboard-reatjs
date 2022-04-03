405b407fbb334d857505e75e9f860e86aa26a033
Prashant Pillai <ppillai@Prashants-MacBook-Air-2.local>
2022-04-03 18:28:15 +0530

    Designed form page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Link,
   BrowserRouter
 } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
 
 import Home from "./pages/home/Home"
 import List from "./pages/list/List"
@@ -23,12 +24,12 @@ function App() {
             <Route path="users">
               <Route index element={<List/>}/>
               <Route path=":userId" element={<Single/>}/>
-              <Route path="new" element={<New/>}/>
+              <Route path="new" element={<New inputs={userInputs} title="Add New User"/>}/>
             </Route>
             <Route path="products">
               <Route index element={<List/>}/>
               <Route path=":productId" element={<Single/>}/>
-              <Route path="new" element={<New/>}/>
+              <Route path="new" element={<New inputs={productInputs} title="Add New Product"/>}/>
             </Route>
           </Route>  
         </Routes>
